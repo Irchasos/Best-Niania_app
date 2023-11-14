1a846ee48dda66c1438f98bacb96572e54ac130f
Irchasos <arkadiusz.holub@podkarpacie.eu>
2023-11-14 18:03:44 +0100

    first

---
+++ database/migrations/2021_11_08_221143_create_user_guardian_pivot_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateuserGuardianPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_guardians', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('guardian_id')->index();
+            $table->foreign('guardian_id')->references('id')->on('guardians')->onDelete('cascade');
+            $table->primary(['user_id', 'guardian_id']);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_guardians');
+    }
+}
