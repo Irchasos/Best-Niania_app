acb70212d9859d8200f97782ee1531a23cfe3298
Irchasos <arkadiusz.holub@podkarpacie.eu>
2023-11-15 19:30:08 +0100

    Set district Edit

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Option extends Model
 {
     use HasFactory;
+    public function guardians()
+    {
+        return $this->belongsToMany(Guardian::class, 'guardian_options', 'guardian_id', 'option_id');
+    }
+
 }
--- app/Models/OptionGuardian.php
+++ app/Models/OptionGuardian.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class OptionGuardian extends Model
 {
     use HasFactory;
+    protected $casts = [
+        'guardian_id' => 'integer',
+        'district_id' => 'integer',
+    ];
 }
